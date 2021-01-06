d9c3a494d523b92809c484a8ab0fccd063fe6dda
Joel Alejandro Montalvo Salazar <joel.montalvo.salazar@estudiantes.cordillera.edu.ec>
2021-01-06 18:13:33 -0500

    Add project files.

---
+++ Controllers/ListItemsController.cs
@@ -0,0 +1,83 @@
+﻿using AspNetWebApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AspNetWebApiRest.Controllers
+{
+    [Authorize]
+    public class ListItemsController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<CustomListItem> Get()
+        {
+            return _listItems;
+        }
+
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody] CustomListItem model)
+        {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var maxId = 0;
+            if (_listItems.Count > 0)
+            {
+                maxId = _listItems.Max(x => x.Id);
+            }
+            model.Id = maxId + 1;
+            _listItems.Add(model);
+            return Request.CreateResponse(HttpStatusCode.Created, model);
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage Put(int id, [FromBody] CustomListItem model)
+        {
+            if (string.IsNullOrEmpty(model?.Text))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                // Actualiza *todas* las propiedades de los items
+                item.Text = model.Text;
+                /*item.Marca = model.Marca;
+                item.Cantidad = model.Cantidad;
+                item.Precio = model.Precio;*/
+
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(int id)
+        {
+            var item = _listItems.FirstOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _listItems.Remove(item);
+                return Request.CreateResponse(HttpStatusCode.OK, item);
+            }
+            return Request.CreateResponse(HttpStatusCode.NotFound);
+        }
+
+        private static List<CustomListItem> _listItems { get; set; } = new List<CustomListItem>();
+    }
+}
---
+++ Models/CustomListItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApiRest.Models
+{
+    public class CustomListItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        /*public string Descripcion { get; set; }
+        public string Marca { get; set; }
+        public int Cantidad { get; set; }
+        public double Precio { get; set; }*/
+    }
+}
